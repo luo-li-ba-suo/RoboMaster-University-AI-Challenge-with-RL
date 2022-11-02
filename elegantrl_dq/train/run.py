b646909038e53595caf4b516fe3a8cf97672d70b
luo-li-ba-suo <luo_li_ba_suo@163.com>
2022-11-02 18:31:16 +0800

    完善wandb_job

--- elegantrl_dq/train/run.py
+++ elegantrl_dq/train/run.py
@@ -97,7 +97,8 @@ class Arguments:
         robot_b_num = self.config.env_config['robot_b_num']
         red_agent = [name.split('.')[-1] for name in self.config.env_config['red_agents_path']]
         blue_agent = [name.split('.')[-1] for name in self.config.env_config['blue_agents_path']]
-        self.config.wandb_job_type = str(robot_r_num) + ''.join(red_agent) + '_vs_' + str(robot_b_num) + ''.join(blue_agent)
+        eval_blue_agent = [name.split('.')[-1] for name in self.config.env_config['eval_blue_agents_path']]
+        self.config.wandb_job_type = str(robot_r_num) + '+'.join(red_agent) + '_vs_' + str(robot_b_num) + '+'.join(blue_agent) + '_eval_' + '+'.join(eval_blue_agent)
         self.config.wandb_name += '_selfPlay' if self.config.self_play else ''
 
         # ppo
